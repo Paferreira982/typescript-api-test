be1fdc99bd15778b651fdb8981d6f988e16b3299
Paferreira982 <paferreira982@hotmail.com>
2022-04-17 02:02:36 -0300

    Documentação simples da interface de user

--- src/app/interfaces/IUser.ts
+++ src/app/interfaces/IUser.ts
@@ -1,3 +1,4 @@
+// INTERFACE PARA AUXÍLIO DE MÉTODOS DE USER //
 export interface IUser {
     id?: number
     name: string
@@ -8,6 +9,7 @@ export interface IUser {
     roles?: string[]
 }
 
+// INTERFACE PARA AUXÍLIO NA CONSTRUÇÃO DE MÉTODOS DE UPDATE DE USER //
 export interface IUserUpdate {
     name?: string
     username?: string

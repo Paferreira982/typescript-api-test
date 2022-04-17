7cd719aabc0ffd03e145e401e55963910330d2d8
Paferreira982 <paferreira982@hotmail.com>
2022-04-17 02:03:50 -0300

    Documentação simples da interface de controllers

--- src/app/interfaces/IController.ts
+++ src/app/interfaces/IController.ts
@@ -1,5 +1,6 @@
 import { Request, Response } from 'express'
 
+// INTERFACE PARA AUXÍLIO NA CONSTRUÇÃO DE CLASSES DE CONTROLLER //
 export interface IController {
     create(req: Request, res: Response): Promise<Response>
     update(req: Request, res: Response): Promise<Response>
